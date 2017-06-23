5b7c0ae16404053cf6476aa87242636c02a865c5
Lauren <lauren.risberg@gmail.com>
2017-06-22 18:44:45 -0600

    nav bar rendering to splash page

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const appName = (
+  <div className="container">
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">Well Spent</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+        </div>
+      </div>
+    </nav>
+  </div>
+);
+
+ReactDOM.render(
+  appName,
+  document.getElementById('root')
+);
