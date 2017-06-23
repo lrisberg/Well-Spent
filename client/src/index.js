ea39ef1c4a5bf30c6fdc11c4038a6880a3fe7596
Lauren <lauren.risberg@gmail.com>
2017-06-23 11:13:58 -0600

    landing page changed to a component

--- client/src/index.js
+++ client/src/index.js
@@ -8,35 +8,46 @@ import {
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const landingPage = (
-  <div className="container">
-    <div class="header clearfix">
-      <nav>
-        <ul className="nav nav-pills pull-right">
-          <li role="presentation" className="active">
-            <a href="#">Home</a>
-          </li>
-          <li role="presentation">
-            <a href="#">About</a>
-          </li>
-          <li role="presentation">
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <h3 className="text-muted">Well Spent</h3>
-    </div>
-    <div className="jumbotron">
-      <h1>It's not how much money you have</h1>
-      <p className="lead">It's how you spend it.</p>
-      <p>
-        <a className="btn btn-lg btn-success" href="#" role="button">Sign Up</a>
-      </p>
-    </div>
-  </div>
-);
+class LandingPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      shown: true,
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="header clearfix">
+          <nav>
+            <ul className="nav nav-pills pull-right">
+              <li role="presentation" className="active">
+                <a href="#">Home</a>
+              </li>
+              <li role="presentation">
+                <a href="#">About</a>
+              </li>
+              <li role="presentation">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <h3 className="text-muted">Well Spent</h3>
+        </div>
+        <div className="jumbotron">
+          <h1>It's not how much money you have</h1>
+          <p className="lead">It's how you spend it.</p>
+          <p>
+            <a className="btn btn-lg btn-success" href="#" role="button">Sign Up</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  landingPage,
+  <LandingPage />,
   document.getElementById('root')
 );
