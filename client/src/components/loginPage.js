625eaa761bcd050113a12e269673ee1b6c4d2a96
Lauren <lauren.risberg@gmail.com>
2017-06-24 11:42:43 -0600

    logging in makes a request

--- client/src/components/loginPage.js
+++ client/src/components/loginPage.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  handleChangePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    axios.post('/api/tokens', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>Email address</label>
-            <input type="email" className="form-control" placeholder="Email" />
+            <input type="email" value={this.state.email} onChange={this.handleChangeEmail} className="form-control" placeholder="Email" />
           </div>
           <div className="form-group">
             <label>Password</label>
-            <input type="password" className="form-control" placeholder="Password" />
+            <input type="password" value={this.state.password} onChange={this.handleChangePassword} className="form-control" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-default">Log In</button>
         </form>
