0b69e1f0e3e52d3449f926ce81345cb30a0d53c3
Lauren <lauren.risberg@gmail.com>
2017-06-24 12:02:40 -0600

    return jwt

--- routes/tokens.js
+++ routes/tokens.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.JWT_KEY || 'its a secret SHHHHHH!';
 
 router.post('/', function(req, res, next) {
   let password = req.body.password;
@@ -20,8 +23,11 @@ router.post('/', function(req, res, next) {
       }
       bcrypt.compare(password, users[0].hashed_password)
       .then(() => {
-        res.send('ok!')
-        //return token
+        const token = jwt.sign({
+          userId: users[0].id
+        }, SECRET);
+        res.setHeader('Content-Type', 'application/json')
+        res.send(token);
       }, () => {
         res.setHeader('Content-Type', 'plain/text');
         res.status(400);
