5d57d5dab3e48d738c97e23de1411f91c7519932
Lauren <lauren.risberg@gmail.com>
2017-06-25 14:43:10 -0600

    more auth

--- routes/happiness.js
+++ routes/happiness.js
@@ -3,18 +3,31 @@ var router = express.Router();
 const knex = require('../knex');
 
 const checkAuth = require('../common/auth.js').checkAuth;
+const responses = require('../common/responses.js');
 
 router.post('/:purchaseId/happiness/', checkAuth, (req, res, next) => {
-
-  knex('happiness')
-    .returning(['id', 'purchase_id', 'happiness'])
-    .insert({
-      purchase_id: req.params.purchaseId,
-      happiness: req.body.happiness
+  let userId = req.user.userId;
+  knex ('purchases')
+    .where('id', req.params.purchaseId)
+    .then((purchases) => {
+      if (purchases.length === 0) {
+        responses.notFound(res);
+        return;
+      }
+      if (purchases[0].user_id !== userId) {
+        responses.unauthorized(res);
+        return;
+      }
+      knex('happiness')
+        .returning(['id', 'purchase_id', 'happiness'])
+        .insert({
+          purchase_id: req.params.purchaseId,
+          happiness: req.body.happiness
+        })
+        .then((happiness) => {
+          res.send(happiness[0])
+        });
     })
-    .then((happiness) => {
-      res.send(happiness[0])
-    });
 });
 
 module.exports = router;
