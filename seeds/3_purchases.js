3d6c028e863095f99877baad5ab8e9a6fb2bebb9
Lauren <lauren.risberg@gmail.com>
2017-06-27 11:12:54 -0600

    add better seed data

--- seeds/3_purchases.js
+++ seeds/3_purchases.js
@@ -8,8 +8,28 @@ exports.seed = function(knex, Promise) {
             id: 1,
             user_id: 1,
             category_id: 8,
-            name: 'Swedish Fish',
-            price: '3.45',
+            name: 'Vanilla Soy Latte',
+            price: '5.10',
+            date: new Date('2017-04-15 16:26:16 UTC'),
+            created_at: new Date('2017-04-15 14:26:16 UTC'),
+            updated_at: new Date('2017-04-15 14:26:16 UTC')
+          },
+          {
+            id: 2,
+            user_id: 1,
+            category_id: 7,
+            name: 'Running Shoes',
+            price: '112.87',
+            date: new Date('2017-06-24 16:26:16 UTC'),
+            created_at: new Date('2017-06-24 14:26:16 UTC'),
+            updated_at: new Date('2017-06-24 14:26:16 UTC')
+          },
+          {
+            id: 3,
+            user_id: 1,
+            category_id: 5,
+            name: 'Vitamix Blender',
+            price: '299.95',
             date: new Date('2017-06-24 16:26:16 UTC'),
             created_at: new Date('2017-06-24 14:26:16 UTC'),
             updated_at: new Date('2017-06-24 14:26:16 UTC')
--- seeds/4_happiness.js
+++ seeds/4_happiness.js
@@ -15,22 +15,141 @@ exports.seed = function(knex, Promise) {
             id: 2,
             purchase_id: 1,
             happiness: 6,
-            created_at: new Date('2017-06-23 15:26:16 UTC'),
-            updated_at: new Date('2017-06-23 15:26:16 UTC')
+            created_at: new Date('2017-06-16 15:26:16 UTC'),
+            updated_at: new Date('2017-06-16 15:26:16 UTC')
           },
           {
             id: 3,
             purchase_id: 1,
             happiness: 3,
-            created_at: new Date('2017-06-24 15:26:16 UTC'),
-            updated_at: new Date('2017-06-24 15:26:16 UTC')
+            created_at: new Date('2017-06-17 15:26:16 UTC'),
+            updated_at: new Date('2017-06-17 15:26:16 UTC')
           },
           {
             id: 4,
             purchase_id: 1,
+            happiness: 3,
+            created_at: new Date('2017-06-18 15:26:16 UTC'),
+            updated_at: new Date('2017-06-18 15:26:16 UTC')
+          },
+          {
+            id: 5,
+            purchase_id: 1,
+            happiness: 3,
+            created_at: new Date('2017-06-19 15:26:16 UTC'),
+            updated_at: new Date('2017-06-19 15:26:16 UTC')
+          },
+          {
+            id: 6,
+            purchase_id: 1,
+            happiness: 2,
+            created_at: new Date('2017-06-20 15:26:16 UTC'),
+            updated_at: new Date('2017-06-20 15:26:16 UTC')
+          },
+          {
+            id: 7,
+            purchase_id: 1,
+            happiness: 2,
+            created_at: new Date('2017-06-21 15:26:16 UTC'),
+            updated_at: new Date('2017-06-21 15:26:16 UTC')
+          },
+          {
+            id: 8,
+            purchase_id: 2,
+            happiness: 6,
+            created_at: new Date('2017-06-15 15:26:16 UTC'),
+            updated_at: new Date('2017-06-15 15:26:16 UTC')
+          },
+          {
+            id: 9,
+            purchase_id: 2,
+            happiness: 4,
+            created_at: new Date('2017-06-16 15:26:16 UTC'),
+            updated_at: new Date('2017-06-16 15:26:16 UTC')
+          },
+          {
+            id: 10,
+            purchase_id: 2,
+            happiness: 2,
+            created_at: new Date('2017-06-17 15:26:16 UTC'),
+            updated_at: new Date('2017-06-17 15:26:16 UTC')
+          },
+          {
+            id: 11,
+            purchase_id: 2,
             happiness: 1,
-            created_at: new Date('2017-06-25 15:26:16 UTC'),
-            updated_at: new Date('2017-06-25 15:26:16 UTC')
+            created_at: new Date('2017-06-18 15:26:16 UTC'),
+            updated_at: new Date('2017-06-18 15:26:16 UTC')
+          },
+          {
+            id: 12,
+            purchase_id: 2,
+            happiness: 2,
+            created_at: new Date('2017-06-19 15:26:16 UTC'),
+            updated_at: new Date('2017-06-19 15:26:16 UTC')
+          },
+          {
+            id: 13,
+            purchase_id: 2,
+            happiness: 1,
+            created_at: new Date('2017-06-20 15:26:16 UTC'),
+            updated_at: new Date('2017-06-20 15:26:16 UTC')
+          },
+          {
+            id: 14,
+            purchase_id: 2,
+            happiness: 3,
+            created_at: new Date('2017-06-21 15:26:16 UTC'),
+            updated_at: new Date('2017-06-21 15:26:16 UTC')
+          },
+          {
+            id: 15,
+            purchase_id: 3,
+            happiness: 7,
+            created_at: new Date('2017-06-15 15:26:16 UTC'),
+            updated_at: new Date('2017-06-15 15:26:16 UTC')
+          },
+          {
+            id: 16,
+            purchase_id: 3,
+            happiness: 7,
+            created_at: new Date('2017-06-16 15:26:16 UTC'),
+            updated_at: new Date('2017-06-16 15:26:16 UTC')
+          },
+          {
+            id: 17,
+            purchase_id: 3,
+            happiness: 7,
+            created_at: new Date('2017-06-17 15:26:16 UTC'),
+            updated_at: new Date('2017-06-17 15:26:16 UTC')
+          },
+          {
+            id: 18,
+            purchase_id: 3,
+            happiness: 6,
+            created_at: new Date('2017-06-18 15:26:16 UTC'),
+            updated_at: new Date('2017-06-18 15:26:16 UTC')
+          },
+          {
+            id: 19,
+            purchase_id: 3,
+            happiness: 6,
+            created_at: new Date('2017-06-19 15:26:16 UTC'),
+            updated_at: new Date('2017-06-19 15:26:16 UTC')
+          },
+          {
+            id: 20,
+            purchase_id: 3,
+            happiness: 6,
+            created_at: new Date('2017-06-20 15:26:16 UTC'),
+            updated_at: new Date('2017-06-20 15:26:16 UTC')
+          },
+          {
+            id: 21,
+            purchase_id: 3,
+            happiness: 7,
+            created_at: new Date('2017-06-21 15:26:16 UTC'),
+            updated_at: new Date('2017-06-21 15:26:16 UTC')
           }])
       ]);
     })
