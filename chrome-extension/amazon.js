1b549f82906aa1d5c820a6e2fa9f71211fba4516
Lauren <lauren.risberg@gmail.com>
2017-07-02 14:56:04 -0600

    amazon extension script packaged into chrome extension

---
+++ chrome-extension/amazon.js
@@ -0,0 +1,7 @@
+(function() {
+  let script = document.createElement('script');
+
+  script.setAttribute('src', 'https://wellspent.herokuapp.com/store-scripts/amazon.js');
+
+  document.body.appendChild(script);
+})();
