036d82cfc927e07cf2fab8c6ba1a36f1da6b9f2a
Lauren <lauren.risberg@gmail.com>
2017-07-04 09:43:23 -0600

    improve appearance of signup page

--- client/src/components/signupPage.jsx
+++ client/src/components/signupPage.jsx
@@ -61,17 +61,26 @@ export default class SignupPage extends React.Component {
 
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Email address</label>
-            <input type="email" value={this.state.email} onChange={this.handleChangeEmail} className="form-control" placeholder="Email" />
+        <div className="row">
+          <div className="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <div className="panel panel-default panel-login">
+              <div className="panel-body">
+                <h3 className="text-center">Create a New Account</h3>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" value={this.state.email} onChange={this.handleChangeEmail} className="form-control" placeholder="Email" />
+                  </div>
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" value={this.state.password} onChange={this.handleChangePassword} className="form-control" placeholder="Password" />
+                  </div>
+                  <button disabled={!isValid} type="submit" className="btn btn-primary pull-right">Create Account</button>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" value={this.state.password} onChange={this.handleChangePassword} className="form-control" placeholder="Password" />
-          </div>
-          <button disabled={!isValid} type="submit" className="btn btn-default">Sign Up</button>
-        </form>
+        </div>
       </div>
     );
   };
