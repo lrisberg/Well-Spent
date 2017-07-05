10751cdf3774de88d10bc812b94651315e53cf78
Lauren <lauren.risberg@gmail.com>
2017-07-05 10:00:03 -0600

    change splash page blurb copy

--- client/src/components/splashPage.jsx
+++ client/src/components/splashPage.jsx
@@ -9,8 +9,8 @@ export default class SplashPage extends React.Component {
         <div className="splash-hero">
           <div className="container">
             <div className="hero-body">
-              <h1>It's not how much money you have</h1>
-              <p className="lead">It's how you spend it.</p>
+              <h1>It's not how much you spend</h1>
+              <p className="lead">It's how you spend that matters.</p>
               <p>
                 <Link to="/signup" className="btn btn-lg btn-primary" href="#" role="button">Sign Up</Link>
               </p>
@@ -26,7 +26,7 @@ export default class SplashPage extends React.Component {
                   <h4>Emotive Visualizations</h4>
                 </div>
                 <div className="panel-body">
-                  <p>See your satisfaction with your purchases displayed over time and category. See what makes you happy and what doesn't.</p>
+                  <p>See your satisfaction with your purchases displayed over time and category. See what really makes you happy and what doesn't.</p>
                   <img className="img-thumbnail" src="/images/chart.png" />
                 </div>
               </div>
@@ -37,7 +37,7 @@ export default class SplashPage extends React.Component {
                   <h4>Amazon Integration</h4>
                 </div>
                 <div className="panel-body">
-                  <p>Seamlessly add purchases from Amazon with a click of a button using our Chrome Extension.</p>
+                  <p>Track online purchases easily. Seamlessly add orders from Amazon with a click of a button using the Well Spent Chrome Extension.</p>
                   <img className="img-thumbnail" src="/images/amazon.png" />
                 </div>
               </div>
@@ -45,10 +45,10 @@ export default class SplashPage extends React.Component {
             <div className="col-lg-4">
               <div className="panel panel-default">
                 <div className="panel-heading">
-                  <h4>Built-in Notifications</h4>
+                  <h4>Smart Notifications</h4>
                 </div>
                 <div className="panel-body">
-                  <p>Be notified when it's time to add more data. Never miss a data point again.</p>
+                  <p>Be notified when it's time to add more data. Our timed notification schedules offer you the most insightful graphs.</p>
                   <img className="img-thumbnail" src="/images/notification.png" />
                 </div>
               </div>
